33367b2db48db21bfbe35160798abddb329023fd
jocelynalsdorf <jfrodsla@gmail.com>
2015-04-19 11:11:32 -0700

    modulo test passed

--- lib/pinger.rb
+++ lib/pinger.rb
@@ -1,13 +1,14 @@
 class Fixnum
-	define_method(:pinger) do
+	define_method(:pinger) do	
 		ngame = []
 		game = (1..self).to_a
 		game.each() do |item|
 			if item.%(15) == 0
-			ngame.push("pingpong")
+			ngame.push "pingpong"
 		else
 			ngame.push(item)
 		end
-		ngame
+	end
+ngame
   end
 end
--- spec/pinger_spec.rb
+++ spec/pinger_spec.rb
@@ -8,13 +8,8 @@ describe('Fixnum#pinger') do
     expect((15).pinger()).to(eq([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]))
   end
 
-  it("pinger iterates through the created array and puts the item") do
-    expect((15).pinger()).to(eq([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]))
+  it("pinger iterates through the created array,tests if divisible by 15 and puts pingpong instead of the item if it is") do
+    expect((15).pinger()).to(eq([1,2,3,4,5,6,7,8,9,10,11,12,13,14,"pingpong"]))
   end
-  it("tests if game items are pushed into ngame array") do
-  expect((15).pinger()).to(eq([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]))
-  end
-  it("tests if a number is evenly divisible by 15 and if it is replaces the item with 'pingpong'") do
-  	expect((ngame).pinger()).to(eq([1,2,3,4,5,6,7,8,9,10,11,12,13,14,"pingpong"]))
-  end
+ 
 end
