bc0d6827fd5dc70965c9d38d7e4ac32f4e090ad8
jocelynalsdorf <jfrodsla@gmail.com>
2015-04-19 10:21:48 -0700

    changed approach: created empty array, testing passed for pushing items into array

--- lib/pinger.rb
+++ lib/pinger.rb
@@ -1,8 +1,10 @@
 class Fixnum
 	define_method(:pinger) do
+		ngame = []
 		game = (1..self).to_a
-		game.each() do |num|
-			print num
+		game.each() do |item|
+			ngame.push(item)
 		end
+		ngame
   end
 end
--- spec/pinger_spec.rb
+++ spec/pinger_spec.rb
@@ -3,11 +3,15 @@ require('pinger.rb')
 require('pry')
 
 describe('Fixnum#pinger') do
+	
   it("pinger counts from 1 to a givennumber") do
-    expect((5).pinger()).to(eq([1,2,3,4,5]))
-  end
-  it("pinger iterates through the created array and puts the item") do
-    expect((5).pinger()).to(eq([1,2,3,4,5]))
+    expect((15).pinger()).to(eq([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]))
   end
 
+  it("pinger iterates through the created array and puts the item") do
+    expect((15).pinger()).to(eq([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]))
+  end
+  it("tests if game items are pushed into ngame array") do
+  expect((15).pinger()).to(eq([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]))
+  end
 end
